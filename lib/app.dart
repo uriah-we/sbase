17298c16d794afc14c85ae231a87e598afcf31b6
Uriah <uriah@watchenterprise.com>
2022-05-04 16:45:14 +0800

    supabase + getx

---
+++ lib/app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sbase/app/routes/app_pages.dart';
+import 'package:sbase/shared/initial_binding.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      initialBinding: InitialBinding(),
+      defaultTransition: Transition.fade,
+      opaqueRoute: Get.isOpaqueRouteDefault,
+      popGesture: Get.isPopGestureEnable,
+      initialRoute: AppPages.INITIAL,
+      getPages: AppPages.routes,
+      title: 'Supabase Example',
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/shared/initial_binding.dart
@@ -0,0 +1,10 @@
+import 'package:get/get.dart';
+
+class InitialBinding extends Bindings {
+  @override
+  void dependencies() {
+    // Get.put(ClassName());
+    // Get.lazyPut(() => ClassName());
+    // Get.lazyPut(() => Classname(), tag: 'home');
+  }
+}
